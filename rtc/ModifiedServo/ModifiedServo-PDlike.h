9f28c806a1cb7c44faf55d3bb3d2fbec56dbcf74
Rafael Cisneros <rafael.cisneros@aist.go.jp>
2019-06-03 16:58:53 +0900

    Several versions of ModifiedServo [WIP]

--- rtc/ModifiedServo/ModifiedServo-PDlike.h
+++ rtc/ModifiedServo/ModifiedServo-PDlike.h
@@ -142,13 +142,16 @@ class ModifiedServo  : public RTC::DataFlowComponentBase
   double step;    // current interpolation step
   double nstep;   // number of steps to interpolate references
 
-  size_t dof;
+  size_t dof, loop;
+  unsigned int m_debugLevel;
+  int dummy;
 
   std::string gain_fname;
   std::ifstream gain;
 
   hrp::dvector Pgain, Dgain;
   hrp::dvector q_old, qRef_old;
+  hrp::dvector tau_limit_ratio;
 };
 
 
